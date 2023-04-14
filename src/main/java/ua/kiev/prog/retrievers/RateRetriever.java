e37dd6c2d67011a3e103f62567bf690c379c8822
Vladimir Proshin <VlaShin1986@gmail.com>
2023-04-14 23:07:53 +0300

    added scheduled annotation

--- src/main/java/ua/kiev/prog/retrievers/RateRetriever.java
+++ src/main/java/ua/kiev/prog/retrievers/RateRetriever.java
@@ -3,6 +3,7 @@ package ua.kiev.prog.retrievers;
 import org.apache.catalina.connector.Request;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ua.kiev.prog.json.Rate;
@@ -14,9 +15,10 @@ public class RateRetriever {
 
     private static final String URL = "https://api.apilayer.com/fixer/latest?symbols=uah&base=eur";
     private String key = "mZHFHzYRo0Gg7Grv1siHGFBiP3UBjDTB";
-
-    @Cacheable("rates") // Redis
+    @Scheduled(fixedRate = 60000)
+    @Cacheable(value = "rates") // Redis
     public Rate getRate() {
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("apikey", key);
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
